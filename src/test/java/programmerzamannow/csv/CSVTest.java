006a4707409ca0fb9566025a6abb3a965e0171d3
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-21 08:20:25 +0700

    membaca csv

--- src/test/java/programmerzamannow/csv/CSVTest.java
+++ src/test/java/programmerzamannow/csv/CSVTest.java
@@ -1,11 +1,16 @@
 package programmerzamannow.csv;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.io.Reader;
 import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class CSVTest {
 
@@ -22,4 +27,21 @@ public class CSVTest {
         System.out.println(csv);
 
     }
+
+    @Test
+    void readCSV() throws IOException {
+        Path path = Path.of("sample.csv");
+        Reader reader = Files.newBufferedReader(path);
+
+        CSVParser parser = new CSVParser(reader, CSVFormat.DEFAULT);
+
+        for (CSVRecord record : parser){
+            System.out.println("First Name : "+record.get(0));
+            System.out.println("Last Name : "+record.get(1));
+            System.out.println("Age : "+record.get(2));
+            System.out.println("---");
+        }
+
+    }
 }
+
